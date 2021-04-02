24616ee85854d9ec737f28392d6f94858efbc4ac
Samantha876 <chinsamanthaashley@gmail.com>
2021-04-02 13:32:34 -0400

    meal endpoint edited

--- server/controllers/meal.py
+++ server/controllers/meal.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request
 import json
 
-from ..services.meal import create_meal_service
+from ..services.meal import create_meal_service, generate_meal_plan, previousMeals_service, mealPlan_byCalories
 
 meal = Blueprint('meal', __name__)
 
 @meal.route('/', methods=['POST'])
 def create_meal():
     try:
-        # Extracting recipe attributes
+        # Extracting meal attributes
         json_request = request.json
         image_name = json_request['image']
         calories = json_request['calories']
@@ -18,7 +18,6 @@ def create_meal():
             return json.dumps({ "error_message": "Please to enter all required data"
                               }), 500
         
-
         response = create_meal_service(image_name, calories, servings)
 
         if response is None:
@@ -29,17 +28,57 @@ def create_meal():
         print(e)
         return json.dumps({ "error_message": "Please to enter all required data"
                           }), 500
+    
+
+def generate_meal_plan():
+    try:
+        json_request = request.json
+        meal_type = json_request['meal_type']
+        schedule_date = json_request['schedule_date']
+
+        if (len(meal_type) < 5 or len(schedule_date) < 8  ):
+            return json.dumps({ "error_message": "Please to enter all required data"
+                              }), 500
+
+        response= generate_meal_plan(meal_type, schedule_date)
+
+        if response is None:
+            return '', 500
+
+        return json.dumps(response), 200
+
+
+    except Exception as e:
+        print(e)
+        return json.dumps({ "error_message": "Please to enter all required data"
+                          }), 500
 
 
 
 @meal.route('/',methods=['GET'])
-def recipe_search():
+def previous_meal():
         try:
             json_request = request.json
-            user_id = json_request['user_id']
-            name = json_request['name']
+            #user_id = json_request['user_id']
+            #name = json_request['name']
 
-            response= get_recipes_service(user_id,name)
+            response= previousMeals_service()
+            return json.dumps(response), 200
+        except Exception as e:
+            print(e)
+            return json.dumps({ "error_message": "Unable to retrieve data"
+                            }), 500
+
+
+
+@meal.route('/:calorie_count',methods=['GET'])
+def meal_byCalorieCount():
+        try:
+            json_request = request.json
+            #user_id = json_request['user_id']
+            #name = json_request['name']
+
+            response= mealPlan_byCalories()
             return json.dumps(response), 200
         except Exception as e:
             print(e)
--- server/services/meal.py
+++ server/services/meal.py
@@ -1,10 +1,10 @@
 from ..utils.db import insert_update, select
 
-def create_meal_service(meal_id, image_name, calories, type, servings):
+def create_meal_service(image_name, calories, servings):
     resp = None
     meal_inserted = insert_update(
-        f"""INSERT INTO Meal(meal_id, image_name, calories, type, servings)
-        VALUES({meal_id}, {image_name}, {calories}, {type}, {servings})"""
+        f"""INSERT INTO Meal(image_name, calories, servings)
+        VALUES({image_name}, {calories}, {servings})"""
     )
 
     # Checks that meal is successfully inserted before creating
@@ -19,8 +19,10 @@ def create_meal_service(meal_id, image_name, calories, type, servings):
         for count, type in enumerate(meal_type):
 
             # Performs commit on last meal plan insertion
+        
             if(count+1 ==  type_len):
                 commit = True
+        
 
             mealplan_inserted = insert_update(
                 f"""INSERT INTO Meals_Plans(planned_meal_id, meal_id, meal_type)
@@ -31,8 +33,8 @@ def create_meal_service(meal_id, image_name, calories, type, servings):
             if mealplan_inserted is False:
                 break
 
-    # Creating response object after successfully creating recipe
-    # and creating relationship between all ingredients and newly create recipe
+    # Creating response object after successfully creating meal
+    # and creating relationship between meal plans and newly create meal
     if mealplan_inserted:
         resp = {
             id: meal_inserted.id,
@@ -42,3 +44,68 @@ def create_meal_service(meal_id, image_name, calories, type, servings):
         }
 
     return resp
+
+def generate_meal_plan(meal_type, schedule_date):
+    resp = None
+    mealPlan_insert = insert_update(
+        f"""INSERT INTO Meals_Plans(meal_type, schedule_date)
+        VALUES({meal_date}, {schedule_date})"""
+    )
+
+    plannedMeal_inserted = False
+
+    #type_len=len(meal_type)
+    #commit = False
+    if mealPlan_insert:
+        #for count, type in enumerate(meal_type):
+
+            # Performs commit on last meal plan insertion
+            '''
+            if(count+1 ==  type_len):
+                commit = True
+            '''
+            week_start= select(
+                f"""SELECT DATEADD(DAY, 2 - DATEPART(WEEKDAY, GETDATE()), 
+                CAST(GETDATE() AS DATE)) [Week_Start]"""
+            )
+
+            week_end= select(
+                f"""Select DATEADD(DAY, 8 - DATEPART(WEEKDAY, GETDATE()), 
+                CAST(GETDATE() AS DATE)) [Week_End]"""
+            )
+
+            plannedMeal_inserted = insert_update(
+                f"""INSERT INTO Planned_Meal(planned_meal_id, week_start, week_end)
+                VALUES({mealPlan_insert.id}, {week_start}, {week_end})"""
+                , commit
+            )
+
+            if plannedMeal_inserted is False:
+                break
+
+
+    if plannedMeal_inserted:
+        resp = {
+            id: plannedMeal_inserted.id,
+            id: meal_id: mealPlan_insert.id ,
+            meal_type: meal_type,
+            schedule_date: schedule_date
+        }
+
+    return resp
+
+def previousMeals_service():
+    previous_meals=select(
+        f"""SELECT * from table"""
+    )
+
+    return previous_meals
+
+
+def mealPlan_byCalories():
+    meal_calories=select(
+        f"""SELECT * from table"""
+    )
+
+    return meal_calories
+
