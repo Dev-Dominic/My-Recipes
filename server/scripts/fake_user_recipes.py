beed564b1149e53482f38ea446e2ee4d3f6b1226
Samantha876 <chinsamanthaashley@gmail.com>
2021-04-02 22:59:58 -0400

    fake info generated, changes to the db

--- server/scripts/fake_user_recipes.py
+++ server/scripts/fake_user_recipes.py
@@ -2,31 +2,36 @@ from faker import Faker
 import random
 import hashlib
 from datetime import datetime
-import json
+import json 
 
 fake = Faker()
 
-script=("fake_info.sql","a")
+script=open("fake_info.sql","a")
+
 
 password= "password123"
-password =hashlib.sha256(password)
+password =hashlib.sha256(password.encode('utf-8'))
 emails = set()
 urls=["gmail","outlook","yahoo","aol"]
 
-_file = open('datasets\Recipes.json', 'r')
+_file = open("..\..\datasets\Recipes.json", 'r')
 result = json.loads(_file.read())
 
-for _ in range(5):
-    f_name,l_name=fake.name().split(" ")
+for _ in range(200000):
+    name=fake.name().split(" ")
+    f_name=name[0]
+    l_name=name[1]
     email= f_name + l_name+"@"+random.choice(urls)+".com"
     while email in emails:
-        f_name,l_name=fake.name().split(" ")
+        f_name=name[0]
+        l_name=name[1]
         email= f_name + l_name+"@"+random.choice(urls)+".com"
     
-    add_user="INSERT INTO USER(first_name,last_name,email,password) VALUES({f_name},{l_name},{email},{password})"
+    add_user="INSERT INTO USERS(first_name,last_name,email,password) VALUES('{}','{}','{}','{}');\n".format(f_name,l_name,email,password)
     emails.add(email)
 
-    user_id="SELECT User.id from User ORDER BY User.id DESC LIMIT 1"
+    user_id="""SELECT Users.user_id from Users ORDER BY Users.user_id DESC LIMIT 1"""
+    script.write(add_user)
 
     for _ in range(3):    
         n=random.randint(0,8)
@@ -34,10 +39,18 @@ for _ in range(5):
         instructions= result[n]['steps']
         date=datetime.now()
     
-        add_recipe= "INSERT INTO RECIPES(name,instructions,date_created,user_id) VALUES({name},{instructions},{date},{user_id})"
+        #add_recipe= "INSERT INTO RECIPES(name,instructions,date_created,user_id) VALUES({},{},{},{});\n".format(name,instructions,date,user_id)
+        
+        instruction_len=(len(instructions))
+        for i in range(instruction_len):
+            step= instructions[i]
+
+
+        add_instructions="INSERT INTO INSTRUCTIONS(step) VALUES('{}');\n".format(step)
+        instruction_id= """SELECT instruction_id from instructions ORDER BY instructions.instruction_id DESC LIMIT 1"""
+
+        add_recipe= "INSERT INTO RECIPES(name,instruction_id,date_created,user_id) VALUES('{}',({}),'{}',({}));\n".format(name,instruction_id,date,user_id)
+        script.write(add_instructions)
         script.write(add_recipe)
-
-script.write(add_user)    
-
 
 script.close()
